d0beba817ed35765105a93d9f4c87cae5e99cbfd
demosthenesk <demosthenesk@gmail.com>
2017-11-15 23:43:54 +0200

    get back correct file

--- ipFinder.sh
+++ ipFinder.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 ############################################################################
 ## Copyright (c) 2017 Demosthenes Koptsis <demosthenesk@gmail.com>
@@ -32,21 +32,23 @@ SMTP_SERVER="smtp.gmail.com:587"
 
 #CHANGE USERNAME BEFORE FIRST USAGE
 USERNAME="my-email@gmail.com"
-
 #CHANGE PASSWORD BEFORE FIRST USAGE
 PASSWORD="xxxxxx"
+
 #######################################
 
 #### DO NOT CHANGE AFTER THIS LINE ####
-IP=$("$(/usr/bin/env dig)" +short 'myip.opendns.com' '@resolver1.opendns.com')
+SENDEMAIL=`/usr/bin/which sendemail`
+DIG=`/usr/bin/which dig`
+IP=`$DIG +short myip.opendns.com @resolver1.opendns.com`
 
-"$(/usr/bin/env sendemail)" -l email.log \
--f "${EMAIL}" \
--u "${SUBJECT}" \
--m "${IP}" \
--t "${EMAIL}" \
--s "${SMTP_SERVER}" \
--o 'tls=yes' \
--xu "${USERNAME}" \
--xp "${PASSWORD}"
+$SENDEMAIL -l email.log \
+-f $EMAIL \
+-u $SUBJECT \
+-m $IP \
+-t $EMAIL \
+-s $SMTP_SERVER \
+-o tls=yes \
+-xu $USERNAME \
+-xp $PASSWORD
 
